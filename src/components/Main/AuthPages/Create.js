a7c91eedc32ac97acf2d78145fa7aa4b9b8d1045
iliyan-ivanov <iliyan.ivanov9428@gmail.com>
2021-04-01 19:48:56 +0300

    Connected with Database
    Create for was connected with Firebase -Realtime Database. It was tested and its creating articles

--- src/components/Main/AuthPages/Create.js
+++ src/components/Main/AuthPages/Create.js
@@ -1,6 +1,40 @@
+
 let Create = () => {
+    let url = "https://football-site-13535-default-rtdb.europe-west1.firebasedatabase.app/articles.json";
+
+    let createArticle = (category, title, description, imageURL) => {
+        let article = {
+            category,
+            title,
+            description,
+            imageURL,
+            date: new Date().toJSON().slice(0,10)
+        }
+
+        return fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(article)
+        })
+    }
+
+
+    let onCreateHandler = (e) => {
+        e.preventDefault();
+
+        const { category, title, description, imageURL } = e.target;
+
+        createArticle(category.value, title.value, description.value, imageURL.value);
+
+        title.value = '';
+        description.value = '';
+        imageURL.value = '';
+           }
+
     return (
-        <form className="create-form">
+        <form className="create-form" onSubmit={onCreateHandler}>
             <h1>Create Article</h1>
             <div className="create-div">
                 <div>
@@ -13,13 +47,13 @@ let Create = () => {
                 </div>
 
                 <label htmlFor="title">Article Title</label>
-                <input type="text" className="create-title" placeholder="Article Title" name="title" />
+                <input type="text" className="create-title" id="title" placeholder="Article Title" name="title" />
 
                 <label htmlFor="description">Article Description</label>
-                <textarea className="create-description" placeholder="Article Description..." name="description"></textarea>
+                <textarea className="create-description" id="description" placeholder="Article Description..." name="description"></textarea>
 
-                <label htmlFor="imageUrl">Image url</label>
-                <input type="text" className="create-img" placeholder="Image Url" name="imageUrl" />
+                <label htmlFor="imageURL">Image url</label>
+                <input type="text" className="create-img" id="imageURL" placeholder="Image Url" name="imageURL" />
             </div>
             <button type="submit" className="create-btn">Submit</button>
         </form>
